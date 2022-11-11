8d625c7dc4e840c1bae4040e72462c330f4c4658
Oficina <solangel.002@hotmail.com>
2022-11-11 09:45:29 -0500

    deleting examples

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,19 +1,24 @@
 describe('empty spec', () => {
   it('passes', () => {
-    cy.visit('http://localhost/testsol/testsol/')
+    cy.visit('http://localhost/testsol/')
   })
+  ///estoy esperando que tengan placeholders
   it('must have placeholders', () => {
     cy.get('input').should('have.attr', 'placeholder')
   })
-
+  ///espero que el formulario tenga un botón que vacíe el formulario
   it('.clear() it should have a button that empty the form', () => {
-    // https://on.cypress.io/clear
     cy.get('.action-clear').type('Clear this text')
       .should('have.value', 'Clear this text')
       .clear()
       .should('have.value', '')
   })
-  
+  //espero que al enviar el formulario salga un error al haber campos vacíos
+  it('.submit() - submit a form', () => {
+    cy.get('input').invoke('val','')
+    .next()
+    .should('have.attr', 'help-block')
+  })
 
 
 })
